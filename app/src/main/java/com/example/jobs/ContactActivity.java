dbfad8dd3081491736368ca977947f5f3957f73d
faraj98 <hussainabunamous@gmail.com>
2022-08-20 17:11:14 +0300

    inflat contact screen

--- app/src/main/java/com/example/jobs/ContactActivity.java
+++ app/src/main/java/com/example/jobs/ContactActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class ContactActivity extends AppCompatActivity {
 
     TextView v ;
+    EditText nameOfOwner , phoneOfOwner ;
+    Spinner location ;
+    Button contact_btn ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,10 @@ public class ContactActivity extends AppCompatActivity {
 
         v= findViewById(R.id.tvTitleToolbar);
         v.setText("معلومات التواصل مع صاحب العمل");
+
+        nameOfOwner = findViewById(R.id.owner_name_work);
+        phoneOfOwner = findViewById(R.id.owner_phone_no);
+        location = findViewById(R.id.splocation);
+        contact_btn = findViewById(R.id.contactBtn);
     }
 }
